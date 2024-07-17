034ee8708187461fc8d35fe664c49d3d9aea03d7
AhmadjonMurodullayev <murodullayevahmadjon26@gmail.com>
2024-07-17 10:38:32 +0500

    js

--- app.js
+++ app.js
@@ -55,8 +55,24 @@ console.log(symmetricDifference([1, 2, 3,], [3, 4, 5])); // [1, 2, 4, 5]
 // 4.Berilgan massiv ichida musbat, manfiy va nol 
 // qiymatli raqamlarning foizini hisoblang.
 function calculatePercentages(arr) {
-    // Code here
-}
+    let positive = 0;
+    let negative = 0;
+    let zero = 0;
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i] > 0) {
+        positive++;
+      } else if (arr[i] < 0) {
+        negative++;
+      } else {
+        zero++;
+      }
+    }
+    return {
+      positive: (positive / arr.length) * 100,
+      negative: (negative / arr.length) * 100,
+      zero: (zero / arr.length) * 100,
+    };
+  }
 
 console.log(calculatePercentages([1, -2, 0, 4, -5, 6, 0])); 
 // {positive: 42.86, negative: 28.57, zero: 28.57}
@@ -65,43 +81,49 @@ console.log(calculatePercentages([1, -2, 0, 4, -5, 6, 0]));
 //  unikal belgilar ketma-ketligini yarating.
 
 function findPairs(arr, target) {
-    // Code here
+    let result = [];
+    for (let i = 0; i < arr.length; i++) {
+      for (let j = i + 1; j < arr.length; j++) {
+        if (arr[i] + arr[j] === target) {
+          result.push([arr[i], arr[j]]);
+        }
+      }
+    }
+    return result
 }
 
 console.log(findPairs([1, 2, 3, 4, 5], 6)); // [[1, 5], [2, 4]]
 
 
 
-
-// 6.Berilgan massivdan yig'indisi ma'lum bir qiymatga
-//  teng bo'lgan sonlar juftligini toping.
-
-function findPairs(arr, target) {
-    // Code here
-}
-
-console.log(findPairs([1, 2, 3, 4, 5], 6)); // [[1, 5], [2, 4]]
-
-// 7.Berilgan massiv ichidagi har bir raqamning kvadratini
-//  hisoblang va yangi massiv qaytaring.
+// 6.Berilgan massiv ichidagi har bir raqamning kvadratini
+//  hisoblang va yangi massiv qaytaring.yxzzz
 
 function squareNumbers(arr) {
-    // Code here
+    return arr.map((a) => a * a)
 }
 
 console.log(squareNumbers([1, 2, 3, 4, 5])); // [1, 4, 9, 16, 25]
 
-// 8.Berilgan oraliqda nechta palindrom son borligini toping.
+// 7.Berilgan oraliqda nechta palindrom son borligini toping.
 function countPalindromNumbers(start, end) {
-    // Code here
-}
+    let num = []
+    for (let i = start; i <= end; i++) {
+        let revers = parseInt(i.toString().split('').reverse().join(''));
+        if (i === revers) {
+            num.push(i)
+        }
+    }
+    return num;
+  }
 
 console.log(countPalindromNumbers(1, 100)); // 18 (1, 2, 3, 4, 5, 6, 7, 8, 9, 11, 22, 33, 44, 55, 66, 77, 88, 99)
 
-// 9.Berilgan sonlar qatorini vergul 
+// 8.Berilgan sonlar qatorini vergul 
 // bilan ajratilgan qator ko'rinishida qaytaring.
 function joinWithCommas(arr) {
-    // Code here
+let arr2 =arr.join(" , ")
+return arr2
 }
 
 console.log(joinWithCommas([1, 2, 3, 4, 5])); // "1, 2, 3, 4, 5"
